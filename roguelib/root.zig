d97f478ef5d974b93c215c4b76742e2700fb4ae8
Derek Shute <67981895+DerekShute@users.noreply.github.com>
2025-11-29 19:26:22 +0000

    maptile as separate module

--- roguelib/root.zig
+++ roguelib/root.zig
@@ -5,6 +5,7 @@
 pub const Pos = @import("Pos.zig");
 pub const Region = @import("Region.zig");
 pub const Grid = @import("grid.zig").Grid;
+pub const MapTile = @import("maptile.zig").MapTile;
 pub const MessageLog = @import("MessageLog.zig");
 
 //
@@ -27,36 +28,6 @@ pub const Command = enum {
 };
 
 //
-// Visible thing at map space
-//
-// TODO Future: union with monster types and objects?
-//
-
-pub const MapTile = enum {
-    unknown,
-    floor,
-    wall, // Start of features
-    trap,
-    door,
-    stairs_down,
-    stairs_up, // Last feature
-    gold,
-    player,
-
-    pub fn isFeature(self: MapTile) bool {
-        const s: usize = @intFromEnum(self);
-        return switch (s) {
-            @intFromEnum(MapTile.wall)...@intFromEnum(MapTile.stairs_up) => true,
-            else => false,
-        };
-    }
-
-    pub fn isPassable(self: MapTile) bool {
-        return (self != .wall);
-    }
-};
-
-//
 // Unit Test Breakout
 //
 
@@ -64,6 +35,7 @@ comptime {
     _ = @import("Pos.zig");
     _ = @import("Region.zig");
     _ = @import("grid.zig");
+    _ = @import("maptile.zig");
     _ = @import("MessageLog.zig");
 }
 
