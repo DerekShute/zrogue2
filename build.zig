22f7c7587143b92912e2ecc13bcec0b1a9103a2c
Derek Shute <67981895+DerekShute@users.noreply.github.com>
2026-01-17 22:06:55 +0000

    src becomes linux-cli

--- build.zig
+++ build.zig
@@ -49,7 +49,7 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "zrogue",
         .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
+            .root_source_file = b.path("linux-cli/main.zig"),
             .target = target,
             .optimize = optimize,
             .imports = &.{
