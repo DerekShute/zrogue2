c9f92ebf00a79a8189351b25e3a18d035c052931
Derek Shute <67981895+DerekShute@users.noreply.github.com>
2026-03-27 21:04:20 +0000

    pull out connector from client, give vtable

--- client-server/client-main.zig
+++ client-server/client-main.zig
@@ -13,71 +13,37 @@ const Writer = std.io.Writer;
 
 const Remote = server.Remote;
 
-//
-// Service wrapper
-//
-
-const Service = struct {
-    ncurses: NCurses = undefined,
-    remote: Remote = undefined,
-    peer: net.Address = undefined,
-    // Stats
-    purse: i32 = 0,
-    depth: i32 = 0,
-
-    const Self = @This();
-
-    //
-    // Message write wrappers
-    //
-
-    fn Wrap(comptime T: type, comptime MT: server.MessageType) type {
-        // It's just wrappers all the way down.  This just simplifies the
-        // invocation in the write declaration
-        return struct {
-            pub fn write(self: *Self, msg: T) !void {
-                const r_write = Remote.Write(T, @intFromEnum(MT)).write;
-                try r_write(&self.remote, msg);
-            }
-        };
-    }
-
-    pub fn writeEntryRequest(self: *Self, text: []const u8) !void {
-        const write = Wrap(server.EntryRequest, .entry_request).write;
-        try write(self, .{ .name = text });
-    }
-
-    pub fn writeAction(self: *Self, kind: server.ActionMsg.Type, pos: []const i16) !void {
-        const write = Wrap(server.ActionMsg, .action).write;
-        try write(self, .{ .kind = kind, .x = pos[0], .y = pos[1] });
-    }
-
-    pub fn writeDepart(self: *Self, text: []const u8) !void {
-        const write = Wrap(server.Depart, .depart).write;
-        try write(self, .{ .message = text });
-    }
-
-    pub fn run(self: *Self, allocator: Allocator) !void {
-        try self.remote.run(allocator);
-    }
-
-    // NCurses service routines
-
-    fn refresh(self: *Self) void {
-        self.ncurses.refresh();
-    }
-
-    fn setChar(self: *Self, x: u16, y: u16, c: u8) void {
-        self.ncurses.setChar(x, y, c);
-    }
-
-    fn setText(self: *Self, x: u16, y: u16, s: []const u8) void {
-        self.ncurses.setText(x, y, s);
-    }
-};
+const Connector = @import("Connector.zig");
 
 //
-// Service routines
+// Globals
+//
+
+// NCurses
+var ncurses: NCurses = undefined;
+
+// Stats
+var purse: i32 = 0;
+var depth: i32 = 0;
+
+//
+// NCurses minutia
+//
+
+fn refresh() void {
+    ncurses.refresh();
+}
+
+fn setChar(x: u16, y: u16, c: u8) void {
+    ncurses.setChar(x, y, c);
+}
+
+fn setText(x: u16, y: u16, s: []const u8) void {
+    ncurses.setText(x, y, s);
+}
+
+//
+// Display service routines
 //
 
 fn mapToChar(ch: server.MapUpdate.MapTile) u8 {
@@ -114,7 +80,7 @@ fn renderChar(tile: server.MapUpdate.Tile) u8 {
     return mapToChar(tile.floor);
 }
 
-fn displayMessageLine(service: *Service, message: []const u8) void {
+fn displayMessageLine(message: []const u8) void {
     var buf: [80]u8 = undefined;
 
     // TODO not great, should store messages here, not in Client
@@ -122,104 +88,59 @@ fn displayMessageLine(service: *Service, message: []const u8) void {
     @memset(buf[0..], ' ');
     // We know that error.NoSpaceLeft can't happen here
     _ = std.fmt.bufPrint(&buf, "{s}", .{message}) catch unreachable;
-    service.setText(0, 0, buf[0..]);
-    service.refresh();
+    setText(0, 0, buf[0..]);
+    refresh();
 }
 
-fn displayStatLine(service: *Service) void {
+fn displayStatLine() void {
     // msg("Level: %d  Gold: %-5d  Hp: %*d(%*d)  Str: %2d(%d)  Arm: %-2d  Exp: %d/%ld  %s", ...)
     var buf: [80]u8 = undefined;
 
     const fmt = "Level: {}  Gold: {:<5}  Hp: some";
-    const u_purse: u32 = @intCast(service.purse);
-    const output = .{ service.depth, u_purse };
+    const u_purse: u32 = @intCast(purse);
+    const output = .{ depth, u_purse };
 
     @memset(buf[0..], ' ');
     // We know that error.NoSpaceLeft can't happen here
     _ = std.fmt.bufPrint(&buf, fmt, output) catch unreachable;
-    service.setText(0, 25, buf[0..]); // TODO line number
-    service.refresh();
+    setText(0, 25, buf[0..]); // TODO line number
+    refresh();
 }
 
 //
-// State machine callbacks
+// Connector Interface
 //
 
-fn doAction(ctx: *anyopaque, ptr: *anyopaque) Remote.Error!void {
-    _ = ctx;
-    _ = ptr;
-    return error.Failed;
+fn updateMap(x: i16, y: i16, tile: server.MapUpdate.Tile) void {
+    setChar(@intCast(x), @intCast(y + 1), renderChar(tile));
+    refresh();
 }
 
-fn doCommand(ctx: *anyopaque, ptr: *anyopaque) Remote.Error!void {
-    _ = ctx;
-    _ = ptr;
-    return error.Failed;
+fn updateMessage(text: []const u8) void {
+    displayMessageLine(text);
 }
 
-fn doDepart(ctx: *anyopaque, ptr: *anyopaque) Remote.Error!void {
-    _ = ctx;
-    _ = ptr;
-    return error.Failed; // TODO: elegance
-}
+fn updateTable(table: []const u8, entry: []const u8, value: []const u8) void {
+    // TODO: ugh errors
+    const val: i16 = std.fmt.parseInt(i16, value, 10) catch return;
 
-fn doEntryRequest(ctx: *anyopaque, ptr: *anyopaque) Remote.Error!void {
-    _ = ctx;
-    _ = ptr;
-    return error.Failed;
-}
+    // TODO: assuming table is "stats", and this should be generalized
+    _ = table;
 
-//
-// Valid messages
-//
-
-fn doMapUpdate(ctx: *anyopaque, ptr: *anyopaque) Remote.Error!void {
-    const service: *Service = @ptrCast(@alignCast(ctx));
-    const msg: *server.MapUpdate = @ptrCast(@alignCast(ptr));
-
-    service.setChar(
-        @intCast(msg.x),
-        @intCast(msg.y + 1),
-        renderChar(msg.tile),
-    );
-    service.refresh();
-}
-
-fn doMessage(ctx: *anyopaque, ptr: *anyopaque) Remote.Error!void {
-    const service: *Service = @ptrCast(@alignCast(ctx));
-    const msg: *server.Message = @ptrCast(@alignCast(ptr));
-    displayMessageLine(service, msg.message);
-}
-
-fn doTableUpdate(ctx: *anyopaque, ptr: *anyopaque) Remote.Error!void {
-    const service: *Service = @ptrCast(@alignCast(ctx));
-    const msg: *server.TableUpdate = @ptrCast(@alignCast(ptr));
-    const val: i16 = std.fmt.parseInt(i16, msg.value, 10) catch return error.Failed;
-
-    // TODO: assuming table is "stats"
-
-    if (std.mem.eql(u8, "purse", msg.entry)) {
-        service.purse = val;
-    } else if (std.mem.eql(u8, "depth", msg.entry)) {
-        service.depth = val;
+    if (std.mem.eql(u8, "purse", entry)) {
+        purse = val;
+    } else if (std.mem.eql(u8, "depth", entry)) {
+        depth = val;
     }
 
-    displayStatLine(service);
+    displayStatLine();
 }
 
-//
-// Dispatch table for server run
-//
-const fns = [_]Remote.ReadFn{
-    doAction,
-    doCommand,
-    doDepart,
-    doEntryRequest,
-    doMapUpdate,
-    doMessage,
-    doTableUpdate,
+var vt = Connector.VTable{
+    .updateMap = updateMap,
+    .updateMessage = updateMessage,
+    .updateTable = updateTable,
 };
-const rig = server.genDispatch(fns);
 
 //
 // Main
@@ -238,27 +159,20 @@ fn run_game(peer: net.Address) !void {
     var reader = stream.reader(rbuf);
     var writer = stream.writer(&.{});
 
-    var curses = try NCurses.init();
-    defer curses.deinit();
-
-    var service = Service{
-        .ncurses = curses,
+    var connect = Connector{
+        .vt = &vt,
         .peer = peer,
         .remote = Remote{
             .reader = reader.interface(),
             .writer = &writer.interface,
-            .sm = &rig,
         },
     };
-    service.remote.ctx = &service;
 
-    // TODO handle errors
-    // TODO player name
-    try service.writeEntryRequest("anonymous");
-
-    while (true) {
-        try service.run(allocator);
-    }
+    //
+    // Disappear into the connector handler
+    //
+    // TODO: thread
+    try connect.run(allocator);
 }
 
 pub fn main() !void {
@@ -274,7 +188,11 @@ pub fn main() !void {
 
     const port = try std.fmt.parseInt(u16, port_value, 10);
     const peer = try net.Address.parseIp4("127.0.0.1", port);
+
     std.debug.print("Connecting to {f}\n", .{peer});
+
+    ncurses = try NCurses.init();
+    defer ncurses.deinit();
 
     run_game(peer) catch {};
 
