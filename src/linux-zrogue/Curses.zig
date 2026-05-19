9ad991534378e19cc0396ad7dec458cf0adacdb1
Derek Shute <67981895+DerekShute@users.noreply.github.com>
2026-05-19 20:02:34 +0000

    put help menu back

--- src/linux-zrogue/Curses.zig
+++ src/linux-zrogue/Curses.zig
@@ -16,19 +16,6 @@ const Rogue = @import("rogueui").Rogue; // Presentation
 const Self = @This();
 
 //
-// Service Routines
-//
-
-fn renderMap(self: *Self) void {
-    _ = self;
-
-    // TODO: when you need to redisplay from scratch
-
-    // Row 0 is message line, last row (23) is stats, so map 0,0 is at
-    // position 0,1
-}
-
-//
 // Members
 //
 
@@ -36,31 +23,7 @@ c: Client = undefined,
 ui: Rogue = undefined,
 
 //
-// Utilities
-//
-
-fn refresh(self: *Self) void {
-    self.ui.displayRefresh();
-}
-
-fn setMapTile(self: *Self, x: u16, y: u16, tile: Client.DisplayTile) void {
-    self.ui.setMapTile(x, y, tile);
-}
-
-fn setMessage(self: *Self, text: []const u8) void {
-    self.ui.setMessage(text);
-}
-
-fn setStat(self: *Self, name: []const u8, value: i32) void {
-    self.ui.setStat(name, value);
-}
-
-fn setText(self: *Self, x: u16, y: u16, s: []const u8) void {
-    self.ui.setText(x, y, s);
-}
-
-//
-// Constructor / Destructor
+// Lifecycle
 //
 
 pub fn init() !Self {
@@ -86,10 +49,6 @@ pub fn deinit(self: *Self) void {
     self.ui.deinit();
 }
 
-//
-// Lifecycle
-//
-
 pub fn client(self: *Self) *Client {
     self.c.ptr = self;
     return &self.c;
@@ -99,56 +58,6 @@ pub fn client(self: *Self) *Client {
 // Input Utility
 //
 
-fn readCommand(self: *Self) Client.Command {
-    return self.ui.readCommand();
-}
-
-//
-// Display Utility
-//
-
-fn displayHelp(self: *Self) void {
-    self.ui.displayHelp();
-    self.refresh();
-}
-
-fn displayStatLine(self: *Self) void {
-    self.ui.displayStatLine();
-}
-
-fn addMessage(self: *Self, msg: []const u8) void {
-    self.ui.setMessage(msg);
-}
-
-fn displayMessage(self: *Self) void {
-    self.ui.displayMessage();
-}
-
-// TODO: when you need to redisplay from scratch
-fn displayScreen(self: *Self) void {
-    //
-    // Top line: messages
-    //
-
-    self.displayMessage();
-
-    //
-    // Bottom line: stat block
-    //
-
-    self.displayStatLine();
-
-    //
-    // Middle: the map
-    //
-
-    renderMap(self);
-
-    // Regenerate display
-
-    self.refresh();
-}
-
 //
 // VTable Methods
 //
@@ -157,40 +66,27 @@ fn displayScreen(self: *Self) void {
 
 fn cursesAddMessage(ptr: *anyopaque, msg: []const u8) void {
     const self: *Self = @ptrCast(@alignCast(ptr));
-    self.addMessage(msg);
-    self.displayMessage();
-    self.refresh();
+    self.ui.setMessage(msg);
+    self.ui.displayMessage();
+    self.ui.displayRefresh();
 }
 
 fn cursesGetCommand(ptr: *anyopaque) !Client.Command {
     const self: *Self = @ptrCast(@alignCast(ptr));
-
-    var cmd = self.readCommand();
-    while (cmd == .help) { // TODO: if cmd==help ?
-        self.displayHelp();
-        // TODO: Refresh display
-        cmd = self.readCommand();
-        // TODO: hit help a second time to rid menu
-    }
-
-    self.addMessage(" ");
-    self.displayMessage();
-    self.refresh();
-
-    return cmd;
+    return self.ui.readCommand();
 }
 
 fn cursesSetMapTile(ptr: *anyopaque, pos: Pos, tile: Client.DisplayTile) void {
     const self: *Self = @ptrCast(@alignCast(ptr));
     self.ui.setMapTile(@intCast(pos.getX()), @intCast(pos.getY()), tile);
-    self.refresh();
+    self.ui.displayRefresh();
 }
 
 fn cursesSetStatInt(ptr: *anyopaque, name: []const u8, value: i32) void {
     const self: *Self = @ptrCast(@alignCast(ptr));
-    self.setStat(name, value);
-    self.displayStatLine();
-    self.refresh();
+    self.ui.setStat(name, value);
+    self.ui.displayStatLine();
+    self.ui.displayRefresh();
 }
 
 //
